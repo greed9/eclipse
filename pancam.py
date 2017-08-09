eeeef19c42b584a007fce68a2228a852a317b624
Bruno Harger Alves <bhazero025@gmail.com>
2017-08-09 17:41:44 -0400

    Update pancam.py

--- pancam.py
+++ pancam.py
@@ -24,30 +24,30 @@ class FileMaker( ):
         self.fileNumber = startFileNum 
         self.filename = self.baseName + '_' + str( self.fileNumber)
         
-    def nextFileName( self):
+    def nextFileName(self):
         filename = self.filename
         self.fileNumber = self.fileNumber + 1
         self.filename = self.baseName + '_' + str( self.fileNumber)
-        return filename
+        return filename + '.jpg'
         
 class FolderMaker ( ):
     # constructing this also makes the first folder and cds to it
-    def __init__( self, baseName, startFolderNum):
+    def __init__(self, baseName, startFolderNum):
         self.baseName = baseName
         self.folderNum = startFolderNum
         self.currFolderName = baseName + '_' + str(self.folderNum)
         if not os.path.isdir(self.currFolderName):
             os.makedirs(self.currFolderName)
-        os.chdir ( self.currFolderName )
+        os.chdir (self.currFolderName )
     
     # Pop up one dir, then create the next dir in the sequence
-    def nextFolder( self):
+    def nextFolder(self):
         os.chdir ( '..' )
         self.folderNum = self.folderNum + 1
-        self.currFolderName = baseName + '_' + str(self.folderNum)
+        self.currFolderName = self.baseName + '_' + str(self.folderNum)
         if not os.path.isdir(self.currFolderName):
             os.makedirs(self.currFolderName)
-        os.chdir ( self.currFolderName )
+        os.chdir (self.currFolderName )
             
         
 class Servo ( ):
@@ -67,34 +67,47 @@ class Servo ( ):
         pi.set_servo_pulsewidth( self.pin, 0 ) # off
 
 def main( ):
-    xAxisGPIOPin = 16 # Check these
-    yAxisGPIOPin = 17 # Check these
-    horizontalPositions = [ 1000, 1500, 2000]
-    verticalPostions = [1000, 1500, 2000]
+    xAxisGPIOPin = 23 # Check these
+    yAxisGPIOPin = 24 # Check these
+    horizontalPositions = [1250, 1500, 1750]
+    verticalPositions = [1250, 1500, 1750]
     horizontal = Servo ( "Xaxis", xAxisGPIOPin, horizontalPositions)
     vertical = Servo ( "Yaxis", yAxisGPIOPin, verticalPositions)
 
+    folder = FolderMaker('images', 0)
+    file = FileMaker('img', 0)
+    for h in range(3):
+        for v in range(3):
+            horizontal.move(h)
+            vertical.move(v)
+            time.sleep(1)
+            camera.capture(file.nextFileName())
+			
+	
+	
     # Test horizontal movement
-    print( "Staring X axis test")
-    horizontal.move( 0 )
-    time.sleep( 1 )
-    horizontal.move( 1 )
-    time.sleep( 1 )
-    horizontal.move( 2 )
-    time.sleep( 1 )
-    horizontal.servoClose ( )
-    print ( "Done X axis test")
+    #print( "Staring X axis test")
+    #horizontal.move( 0 )
+    #time.sleep( 1 )
+    #horizontal.move( 1 )
+    #time.sleep( 1 )
+    #horizontal.move( 2 )
+    #time.sleep( 1 )
+    #horizontal.servoClose ( )
+    #print ( "Done X axis test")
 
     # Test vertical movement
-    print( "Starting Y axis test")
-    vertical.move( 0 )
-    time.sleep( 1 )
-    vertical.move( 1 )
-    time.sleep( 1 )
-    vertical.move( 2 )
-    time.sleep( 1 )
-    vertical.servoClose ( )
-    print( "Done Y axis test")
+    #print( "Starting Y axis test")
+    #vertical.move( 0 )
+    #time.sleep( 1 )
+    #vertical.move( 1 )
+    #time.sleep( 1 )
+    #vertical.move( 2 )
+    #time.sleep( 1 )
+    #vertical.servoClose ( )
+    #print( "Done Y axis test")
+
+   
 
 if __name__ == "__main__":
     main ( )
